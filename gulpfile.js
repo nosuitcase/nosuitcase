a659f318ce2bdd539534dc22d6fa4e02d3ca9fcc
Josh Hanson <joshhanson@Joshs-MacBook-Pro.local>
2017-10-12 17:44:03 -0400

    Format all scss files

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,20 @@
 'use strict';
 
-const gulp         = require('gulp'),
-      gutil        = require('gulp-util'),
-      plumber      = require('gulp-plumber'),
-      sass         = require('gulp-sass'),
-      sourcemaps   = require('gulp-sourcemaps'),
-      autoprefixer = require('gulp-autoprefixer'),
-      cleancss     = require('gulp-clean-css'),
-      jshint       = require('gulp-jshint'),
-      stylish      = require('jshint-stylish'),
-      uglify       = require('gulp-uglify'),
-      concat       = require('gulp-concat'),
-      rename       = require('gulp-rename'),
-      browser      = require('browser-sync').create();
-gulp.task('browser-sync', ['styles', 'scripts'], function () {
+const gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    plumber = require('gulp-plumber'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cleancss = require('gulp-clean-css'),
+    stylish = require('jshint-stylish'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    browser = require('browser-sync').create();
+
+gulp.task('browser-sync', ['styles'], function () {
     browser.init({
-        server:{
+        server: {
             baseDir: "./"
         }
     });
@@ -28,9 +27,14 @@ gulp.task('styles', function () {
             this.emit('end');
         }))
         .pipe(sass())
-        .pipe(rename({suffix: '.min'}))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(cleancss({ compatibility: 'ie11' }))
+        .pipe(concat('styles.min.css'))
         .pipe(gulp.dest('./css/'))
-        .pipe(browser.reload({stream:true}));
+        .pipe(browser.reload({ stream: true }));
 });
 
 gulp.task('fonts', function () {
@@ -42,28 +46,11 @@ gulp.task('fonts', function () {
         .pipe(gulp.dest('./fonts/dist'));
 });
 
-gulp.task('scripts', function () {
-    var files = ['./assets/js/*.js'];
-
-    return gulp.src(files)
-        .pipe(plumber())
-        .pipe(jshint())
-        .pipe(jshint.reporter('jshint-stylish'))
-        .pipe(concat('scripts.js'))
-        .pipe(gulp.dest('./js/dist'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(uglify())
-        .pipe(gulp.dest('./js/dist'))
-        .pipe(browser.reload({stream:true}));
-
-});
-
 gulp.task('default', function () {
-    gulp.start('styles', 'fonts', 'scripts');
+    gulp.start('styles', 'fonts');
 });
 
 gulp.task('watch', ['browser-sync'], function () {
     gulp.watch('./assets/scss/*/*.scss', ['styles']);
-    gulp.watch('./assets/js/*.js', ['scripts']);
     gulp.watch("*.html").on('change', browser.reload);
 });
